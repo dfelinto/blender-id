fff3569121ae1a823250cfa02d5b637bbdc95673
Francesco Siddi <francesco.siddi@gmail.com>
2015-04-03 15:08:51 +0200

    Added db migrations

---
+++ blender-id/manage.py
@@ -0,0 +1,24 @@
+
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate
+from flask.ext.migrate import MigrateCommand
+from application import app
+from application import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_all_tables():
+    """Creates all tables based on imported db models
+    """
+    db.create_all()
+
+@manager.command
+def runserver():
+    """Overrides the default runserver from flask-script
+    """
+    app.run(debug=True, port=8000, host='0.0.0.0')
+
+manager.run()
