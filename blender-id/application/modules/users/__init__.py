42791551d3fb398cf82f1247519f171549c4f14e
Francesco Siddi <francesco.siddi@gmail.com>
2015-04-10 13:11:48 +0200

    Using user_datastore for user retrieval

--- blender-id/application/modules/users/__init__.py
+++ blender-id/application/modules/users/__init__.py
@@ -7,12 +7,16 @@ from application import app
 from application import db
 
 from application.modules.users.model import UsersRestTokens
-from application.modules.users.model import User
 from application.modules.users.model import user_datastore
 
 
 @app.route('/u/identify', methods=['POST'])
 def verify_identity():
+    """Entry point that generates an authentication token, given exsiting
+    and valid username and password. The token can be used as alternative
+    authentication system for REST based services (e.g. Attract).
+    """
+
     username = request.form['username']
     password = request.form['password']
     hostname = request.form['hostname']
@@ -24,7 +28,17 @@ def verify_identity():
     if not user:
         return jsonify(message='User does not exist')
     if not verify_password(password, user.password):
+        # TODO throttle authentication attempts (limit to 3 or 5)
         return jsonify(message='Wrong password')
+
+    # Check if token already exists for that user and hostname
+    user_rest_token = UsersRestTokens.query\
+        .filter_by(user_id=user.id, hostname=hostname)\
+        .first()
+
+    if user_rest_token:
+        db.session.delete(user_rest_token)
+        db.session.commit()
 
     # Make new REST Token
     user_rest_token = UsersRestTokens(
@@ -41,6 +55,10 @@ def verify_identity():
 
 @app.route('/u/validate_token', methods=['POST'])
 def validate_token():
+    """Validate and existing authentication token. This is usually called by
+    a third party (e.g. Attract) every few requests to confirm the identity
+    of a user.
+    """
     token = request.form['token']
     error = False
 
@@ -51,7 +69,7 @@ def validate_token():
         error = True
 
     if token_info:
-        user = User.query.get(token_info.user_id)
+        user = user_datastore.get_user(token_info.user_id)
         if user:
             user_info = {
                 'email': user.email
