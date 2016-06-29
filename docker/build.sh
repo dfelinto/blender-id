0311b20395ffac950a37131b103748a89e112660
Francesco Siddi <francesco.siddi@gmail.com>
2016-06-29 14:49:22 +0200

    Simplify Docker image building

--- docker/build.sh
+++ docker/build.sh
@@ -4,14 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo $DIR
 
-if [[ $1 == 'pro' || $1 == 'dev' ]]; then
-	# Copy requirements.txt into pro folder
-	cp ../requirements.txt $1/requirements.txt
-	# Build image
-	docker build -t armadillica/blender_id_$1 $1
-	# Remove requirements.txt
-	rm $1/requirements.txt
-
-else
-	echo "POS. Your options are 'pro' or 'dev'"
-fi
+# Copy requirements.txt into current folder
+cp ../requirements.txt .
+# Build image
+docker build -t armadillica/blender_id .
+# Remove requirements.txt
+rm requirements.txt
--- docker/manage.sh
+++ docker/manage.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash -e
 
-. /data/venv/bin/activate
-
 # We have to be in the correct directory for Alembic to find its ini file.
 cd /data/git/blender-id/blender-id
-python ./manage.py "$@"
+
+. /data/venv/bin/activate && python manage.py "$@"
