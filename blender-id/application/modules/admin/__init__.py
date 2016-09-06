4df07f05001979e35d3271bc1348449760c89a30
Sybren A. Stüvel <sybren@stuvel.eu>
2016-09-06 15:37:19 +0200

    Cleaned up imports.
    Instead of 'from x.x.x.x import y' is changed to 'import x.x.x.x'.
    This reduces the number of imports, and makes it clear where 'y' is coming
    from at the spot where it is used.
    
    Also, instead of importing 'flask.ext.something', we import
    'flask_something'.

--- blender-id/application/modules/admin/__init__.py
+++ blender-id/application/modules/admin/__init__.py
@@ -1,28 +1,22 @@
-from flask import render_template
+import os
+import hashlib
+import time
+import os.path
+
 from flask import redirect
 from flask import url_for
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext import admin
-from flask.ext import login
-from flask.ext.admin import Admin
-from flask.ext.admin import expose
-from flask.ext.admin import form
-from flask.ext.admin.contrib import sqla
-from flask.ext.admin.contrib.sqla import ModelView
-from flask.ext.admin.base import BaseView
-from flask.ext.admin.model.template import macro
-from flask.ext.security import current_user
-from flask.ext.security.utils import login_user
 
-from werkzeug import secure_filename
+import flask_admin
+import flask_admin.form
+import flask_admin.base
+import flask_admin.contrib.sqla
+import flask_admin.model.template
+
+import flask_login
+import flask_security.utils
+
+from werkzeug.utils import secure_filename
 from jinja2 import Markup
-from wtforms import fields
-from wtforms import validators
-from wtforms import widgets
-from wtforms.fields import SelectField
-from wtforms.fields import TextField
-import os, hashlib, time
-import os.path as op
 
 from application import app
 from application import db
@@ -45,11 +39,11 @@ def _list_items(view, context, model, name):
 def _list_thumbnail(view, context, model, name):
     if not getattr(model,name):  #model.name only does not work because name is a string
         return ''
-    return Markup('<img src="%s">' % url_for('static', 
+    return Markup('<img src="%s">' % url_for('static',
         filename=thumb.thumbnail(getattr(model,name), '50x50', crop='fit')))
 
 # Create directory for file fields to use
-file_path = op.join(op.dirname(__file__), '../../static/files',)
+file_path = os.path.join(os.path.dirname(__file__), '../../static/files',)
 try:
     os.mkdir(file_path)
 except OSError:
@@ -58,7 +52,7 @@ except OSError:
 
 def prefix_name(obj, file_data):
     # Collect name and extension
-    parts = op.splitext(file_data.filename)
+    parts = os.path.splitext(file_data.filename)
     # Get current time (for unique hash)
     timestamp = str(round(time.time()))
     # Has filename only (not extension)
@@ -69,7 +63,7 @@ def prefix_name(obj, file_data):
 
 
 def image_upload_field(label):
-    return form.ImageUploadField(
+    return flask_admin.form.ImageUploadField(
         label,
         base_path=file_path,
         thumbnail_size=(100, 100, True),
@@ -79,24 +73,24 @@ def image_upload_field(label):
 
 
 # Create customized views with access restriction
-class CustomModelView(ModelView):
+class CustomModelView(flask_admin.contrib.sqla.ModelView):
     def is_accessible(self):
-        return login.current_user.has_role('admin')
+        return flask_login.current_user.has_role('admin')
 
-class CustomBaseView(BaseView):
+class CustomBaseView(flask_admin.base.BaseView):
     def is_accessible(self):
-        return login.current_user.has_role('admin')
+        return flask_login.current_user.has_role('admin')
 
 
 class UserOperationsView(CustomBaseView):
-    @expose('/')
+    @flask_admin.expose('/')
     def index(self):
         return redirect(url_for('homepage'))
 
-    @expose('/login/<int:user_id>/')
+    @flask_admin.expose('/login/<int:user_id>/')
     def login(self, user_id):
         user = user_datastore.get_user(user_id)
-        login_user(user)
+        flask_security.utils.login_user(user)
         return redirect(url_for('homepage'))
 
 
@@ -104,33 +98,33 @@ class UserView(CustomModelView):
     can_delete = False
     column_list = ('email', 'active', 'full_name', 'user_operations')
     column_filters = ('id', 'email', 'active', 'full_name')
-    column_formatters = dict(user_operations=macro('user_operations'))
+    column_formatters = dict(user_operations=flask_admin.model.template.macro('user_operations'))
     form_columns = ('email', 'full_name', 'active', 'roles')
     list_template = 'admin/user/list.html'
 
 
 # Create customized index view class that handles login & registration
-class MyAdminIndexView(admin.AdminIndexView):
+class MyAdminIndexView(flask_admin.AdminIndexView):
 
-    @expose('/')
+    @flask_admin.expose('/')
     def index(self):
-        if login.current_user.has_role('admin'):
+        if flask_login.current_user.has_role('admin'):
             return self.render(
                 'admin/homepage.html')
         else:
             return redirect(url_for('homepage'))
 
-    @expose('/logout/')
+    @flask_admin.expose('/logout/')
     def logout_view(self):
-        login.logout_user()
+        flask_login.logout_user()
         return redirect(url_for('homepage'))
 
 
 # Create admin backend
-backend = admin.Admin(
-    app, 
-    'Blender ID', 
-    index_view=MyAdminIndexView(), 
+backend = flask_admin.Admin(
+    app,
+    'Blender ID',
+    index_view=MyAdminIndexView(),
     base_template='admin/layout_admin.html'
 )
 
